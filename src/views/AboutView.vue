a8aa39fe1fa4e938b0960808bffb89cfba309371
yoomster <nperenboom@hotmail.com>
2025-05-07 22:49:07 +0200

    minor edits

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+      consequuntur tempore assumenda et accusantium architecto iusto fugit
+      numquam ex vitae unde perspiciatis quis commodi optio quo sint nemo sequi
+      blanditiis atque eos. Modi accusantium et aspernatur quo voluptatem, dicta
+      nesciunt doloremque doloribus repellat. Amet laborum minima dolore animi
+      consequuntur asperiores aut voluptas praesentium earum! Debitis, culpa
+      voluptatibus, quae iure aperiam fuga obcaecati temporibus perferendis
+      repellendus quas neque alias dolor numquam reprehenderit quasi repudiandae
+      ratione, quia enim saepe dicta architecto quos est corrupti sed. Ipsam, ad
+      totam rem veritatis repudiandae quae quod fuga atque eos sit. Dicta
+      consectetur amet quidem saepe.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Homepage</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+      consequuntur tempore assumenda et accusantium architecto iusto fugit
+      numquam ex vitae unde perspiciatis quis commodi optio quo sint nemo sequi
+      blanditiis atque eos. Modi accusantium et aspernatur quo voluptatem, dicta
+      nesciunt doloremque doloribus repellat. Amet laborum minima dolore animi
+      consequuntur asperiores aut voluptas praesentium earum! Debitis, culpa
+      voluptatibus, quae iure aperiam fuga obcaecati temporibus perferendis
+      repellendus quas neque alias dolor numquam reprehenderit quasi repudiandae
+      ratione, quia enim saepe dicta architecto quos est corrupti sed. Ipsam, ad
+      totam rem veritatis repudiandae quae quod fuga atque eos sit. Dicta
+      consectetur amet quidem saepe.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+      consequuntur tempore assumenda et accusantium architecto iusto fugit
+      numquam ex vitae unde perspiciatis quis commodi optio quo sint nemo sequi
+      blanditiis atque eos. Modi accusantium et aspernatur quo voluptatem, dicta
+      nesciunt doloremque doloribus repellat. Amet laborum minima dolore animi
+      consequuntur asperiores aut voluptas praesentium earum! Debitis, culpa
+      voluptatibus, quae iure aperiam fuga obcaecati temporibus perferendis
+      repellendus quas neque alias dolor numquam reprehenderit quasi repudiandae
+      ratione, quia enim saepe dicta architecto quos est corrupti sed. Ipsam, ad
+      totam rem veritatis repudiandae quae quod fuga atque eos sit. Dicta
+      consectetur amet quidem saepe.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
